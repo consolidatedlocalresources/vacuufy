880b1b989459157d179b608150dc84ade19e5326
Jim Peterson <jamespeterson137@gmail.com>
2019-08-21 21:02:51 -0400

    First Commit - Angular SIde of App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { InputZipcodeDataComponent } from './inputZip/input-zipcode.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputZipcodeDataComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/inputZip/input-zipcode.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-zipcode-data',
+  templateUrl: './input-zipcode.component.html',
+  styleUrls: ['./input-zipcode.component.css']
+})
+
+export class InputZipcodeDataComponent {
+
+  title = 'Enter your zip code: ';
+  zipCode = [6067, 6416, 6457, 6033];
+  myZipCode = this.zipCode[0];
+
+}
