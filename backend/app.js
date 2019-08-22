30bcdcce4eee7ec311a57d9e34f2bd0307afd68f
Jim Peterson <jamespeterson137@gmail.com>
2019-08-22 09:31:41 -0400

    Created error handling within node file and included nodemon

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('First Middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   res.send('Hello from Express!');
 });
 
