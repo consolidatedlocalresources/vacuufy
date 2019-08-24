4a3fd4ce4125430519d28611a0fcd205667aa3a2
Jim Peterson <jamespeterson137@gmail.com>
2019-08-24 11:39:10 -0400

    Connected post-create with post-list and created a post.model

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Post } from '../posts.model';
 
 @Component({
   selector: 'app-post-create',
@@ -7,10 +10,18 @@ import { Component } from '@angular/core';
 })
 
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = 'NO CONTENT';
+  enteredTitle = '';
+  enteredContent = '';
+  @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    this.newPost = this.enteredValue;
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content
+    };
+    this.postCreated.emit(post);
   }
 }
