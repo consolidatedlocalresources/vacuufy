a12c1af20f4c92087b34bcb9d7781d27971fcee5
Jim Peterson <jamespeterson137@gmail.com>
2019-08-26 19:52:18 -0400

    Added Express and Node

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.send('Hello from Express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    { id: 'c507cqf5e70',
+      title: 'First Server-Side Post',
+      content: 'This is where we add some amazing content'
+    },
+    { id: 'qc3g579cq5g8m0',
+      title: 'Second Server-Side Post',
+      content: 'This is where we add even MORE amazing content'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
