c0e3858834fdd331716a69cc3058e231130eb977
Jim Peterson <jamespeterson137@gmail.com>
2019-08-27 06:26:50 -0400

    COnnected database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://jpstrategy:EmM6TNKApGaBlRSt@cluster0-rwgoo.mongodb.net/node-angular?retryWrites=true&w=majority")
+.then(() => {
+  console.log("Connected to database!")
+})
+.catch(() => {
+  console.log("Connection failed!")
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -23,7 +32,7 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
